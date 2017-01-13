7aa78c02df777ad28b47e31d919e3e1bf6bd83af
Craig Zielinski <z@killinski.com>
2017-01-13 14:21:55 -0800

    Started messing around

--- functions.php
+++ functions.php
@@ -107,6 +107,8 @@ function strong_af_scripts() {
 	wp_enqueue_script( 'strong-af-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'strong-af-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	
+	wp_enqueue_script( 'strong-af-myfonts', get_template_directory_uri() . '/js/MyFontsWebfontsKit.js', array(), '20170112', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
