cd2c9ccaf4aea9408e6dca299f9fa6369d1be9b7
Craig Zielinski <z@killinski.com>
2017-02-19 19:30:26 -0800

    spacing and carriage returns

--- js/strong-af-again.js
+++ js/strong-af-again.js
@@ -1,4 +1,4 @@
-function restoreAndSkipContent() {
+function restoreAndSkipContent(){
   var hidden = document.getElementsByClassName( 'skip-me' )[0];
   hidden.classList.add( 'unhide' );
   window.scroll( 0, hidden.offsetHeight );
--- js/strong-af.js
+++ js/strong-af.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($) {  
+jQuery( document ).ready( function( $ ){  
 
 	/* Stick navigation to the top of the page */
 	var stickyNavTop = $( 'section#begin' ).offset().top; 
 
-	$(window).scroll(function(){  
-		var scrollTop = $(window).scrollTop();  
+	$( window ).scroll( function(){  
+		var scrollTop = $( window ).scrollTop();  
 
 		if ( scrollTop > stickyNavTop ) {   
 			$( 'section#begin' ).addClass( 'sticky-header' ); 
@@ -17,8 +17,8 @@ jQuery(document).ready(function($) {
 	});
 	
 	/* Scroll to specific section on front page */
-		$(function() {
-			$( 'a[href*=#]:not([href=#])' ).click( function() {
+		$( function() {
+			$( 'a[href*=#]:not([href=#])' ).click( function(){
 				if ( location.pathname.replace(/^\//,'' ) == this.pathname.replace( /^\//,'' ) && location.hostname == this.hostname ) {
 					var target = $( this.hash );
 					target = target.length ? target : $( '[name=' + this.hash.slice(1) +']' );
